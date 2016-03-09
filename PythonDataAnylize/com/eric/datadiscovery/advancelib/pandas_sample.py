171d4f99d1cf74f1e2498dc62ed118fa14cdfd88
Eric-aihua <eric.sunah@gmail.com>
2016-03-09 21:58:16 +0800

    update to python 3.x

--- PythonDataAnylize/com/eric/datadiscovery/advancelib/pandas_sample.py
+++ PythonDataAnylize/com/eric/datadiscovery/advancelib/pandas_sample.py
@@ -6,11 +6,11 @@ s=Series([1,2,3],index=['a','b','c'])
 d=DataFrame([[1,2,3],[4,5,6]],columns=['a','b','c'])
 
 #head() method will return top 5 records
-print s.head()
-print s.describe()
-print d.head()
-print d.describe()
+print (s.head())
+print (s.describe())
+print (d.head())
+print (d.describe())
 
 #read data from xml file
 excel_data=pd.read_excel("./server.xlsx")
-print excel_data.head
+print (excel_data.head)
