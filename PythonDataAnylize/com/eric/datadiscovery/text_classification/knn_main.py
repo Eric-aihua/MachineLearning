6333783902066a97fc6a9ac1d13a380a65a3a85a
Eric <eric.sunah@gmail.com>
2016-09-13 18:45:21 +0800

    update knn code

--- PythonDataAnylize/com/eric/datadiscovery/text_classification/knn_main.py
+++ PythonDataAnylize/com/eric/datadiscovery/text_classification/knn_main.py
@@ -69,4 +69,5 @@ if __name__ =='__main__':
     # 使用之前贝叶斯分类阶段的数据集，以及生成的 tf 向量进行分类
     test_bunch=read_object(test_data)
     tf_idf_bunch=read_object(tf_idf_space_data)
+    
     classify(test_bunch.tdm,tf_idf_bunch.tdm,tf_idf_bunch.label)
